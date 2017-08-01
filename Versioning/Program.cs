9be0196d69f84d118ada0364c61712a270283734
Andrey Titok <andrey_by@list.ru>
2017-08-01 12:09:11 +0300

    release 1.1.0

--- Versioning/Program.cs
+++ Versioning/Program.cs
@@ -6,12 +6,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("v1.0.0");
-
-            Console.WriteLine("feature 1");
-
-            Console.WriteLine("feature 2");
-            Console.WriteLine("release 1.1.0 small fix");
+            Console.WriteLine("release 1.1.0");
         }
     }
 }
