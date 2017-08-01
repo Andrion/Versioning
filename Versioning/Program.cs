18f835c2f9ae0924be2000c3529e031847569b05
Andrey Titok <andrey_by@list.ru>
2017-08-01 10:59:40 +0300

    v1.0.0

--- Versioning/Program.cs
+++ Versioning/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace Versioning
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("v1.0.0");
         }
     }
 }
